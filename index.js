3499add8feda6cb146a58ad1ee7ca76a06807298
Will Gundy <willgunderson503@gmail.com>
2022-06-07 15:10:13 -0700

    added stack class

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
-class Stack {}
+//LIFO
+class Stack {
+    #stack = [];
 
-class Queue {}
+    constructor(initialStack) {
+        if (initialStack) this.#stack = initialStack;
+    }
+
+    // methods
+    addItem(item) {
+        this.#stack.push(item);
+    }
+
+    removeItem(item) {
+        this.#stack.pop(item);
+    }
+
+    peek() {
+        this.#stack.peekBack();
+    }
+}
+
+//FIFO
+class Queue {
+    #queue = [];
+
+    constructor(initialQueue) {
+        if (initialQueue) this.#queue = initialQueue;
+    }
+
+    //methods
+    enqueue(item) {
+        this.#queue.push(item);
+    }
+
+    dequeue(item) {
+        this.#queue.shift(item);
+    }
+
+    hasNext() {
+        this.#queue.length() > 0 ? true : false;
+    }
+}
 
 module.exports = { Stack, Queue };
